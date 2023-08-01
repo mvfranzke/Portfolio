d6ffd5caa57e04ce42d644c9d179414ecda30636
mvfranzke <michellevonfranzke@gmail.com>
2023-08-01 15:49:11 -0700

    edited container for social media icon to only appear when viewed in large screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
         </ul>
 
         {/* social media icons */}
-        <div className="flex fixed flex-col top-[35%] left-0">
+        <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
           <ul>
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300">
               <a
