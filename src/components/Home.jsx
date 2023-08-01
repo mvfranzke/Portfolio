ef4ededc12a779298a315604308abc295d7d3f2b
mvfranzke <michellevonfranzke@gmail.com>
2023-08-01 15:20:19 -0700

    import additional icons eg. FaFacebook, FaLinkedIn, FaGithub and added icons inside social media portion of the webpage

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+/* import HiArrowNarrowRight icon*/
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars, FaTimes, FaGithub, FaLinkedIn, FaFacebook } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill} from "react-icons/bs";
 import Logo from '../assets/logo1.png'
 
 /* define Navbar variable and set up setter and default state to false, created handleClick function to display list when FaBars is clicked */
@@ -12,43 +14,55 @@ const Navbar = () => {
 
 
     return (
-    <div className="fixed w-full h-[100px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
-      <div>
-        <img src={Logo} alt="name logo" style={{ width: "100px" }} />
-      </div>
+      <div className="fixed w-full h-[100px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+        <div>
+          <img src={Logo} alt="name logo" style={{ width: "100px" }} />
+        </div>
 
-      {/* navbar menu options */}
-      <ul className="hidden md:flex">
-        <li>Home</li>
-        <li>About Me</li>
-        <li>Skills</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
-        <li>Resume</li>
-      </ul>
-
-      {/* additional menu option */}
-      {/* set up ternatry operator, display FaBars if nav is not visible and disply x image for FaTimes */}
-      <div onClick={handleClick} className="md:hidden z-10">
-        {!nav ? <FaBars /> : <FaTimes />}
-      </div>
-
-      {/* Mobile view menu */}
-      {/* ternary operator to display list when FaBars is visible and clicked */}
-        <ul className={ !nav ? 'hidden' : 'absolute top-0 left-1 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-          <li className='py-6 text-4xl'>Home</li>
-          <li className='py-6 text-4xl'>About Me</li>
-          <li className='py-6 text-4xl'>Skills</li>
-          <li className='py-6 text-4xl'>Portfolio</li>
-          <li className='py-6 text-4xl'>Contact</li>
-          <li className='py-6 text-4xl'>Resume</li>''
+        {/* navbar menu options */}
+        <ul className="hidden md:flex">
+          <li>Home</li>
+          <li>About Me</li>
+          <li>Skills</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+          <li>Resume</li>
         </ul>
 
+        {/* additional menu option */}
+        {/* set up ternatry operator, display FaBars if nav is not visible and disply x image for FaTimes */}
+        <div onClick={handleClick} className="md:hidden z-10">
+          {!nav ? <FaBars /> : <FaTimes />}
+        </div>
 
-      {/* social media icons */}
-      <div className="hidden"></div>
-    </div>
-  );
+        {/* Mobile view menu */}
+        {/* ternary operator to display list when FaBars is visible and clicked */}
+        <ul
+          className={
+            !nav
+              ? "hidden"
+              : "absolute top-0 left-1 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+          }
+        >
+          <li className="py-6 text-4xl">Home</li>
+          <li className="py-6 text-4xl">About Me</li>
+          <li className="py-6 text-4xl">Skills</li>
+          <li className="py-6 text-4xl">Portfolio</li>
+          <li className="py-6 text-4xl">Contact</li>
+          <li className="py-6 text-4xl">Resume</li>''
+        </ul>
+
+        {/* social media icons */}
+        <div className="flex fixed flex-col top-[35%] left-0">
+          <ul>
+            <li>
+              <a href="/">LinkedIn <FaFacebook/>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
 }
 
 export default Navbar
