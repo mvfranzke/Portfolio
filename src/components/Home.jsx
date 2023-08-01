b4610bfe517d72496fa8ebc48fd11a5c900599fd
mvfranzke <michellevonfranzke@gmail.com>
2023-08-01 16:40:15 -0700

    set up classname to adjust font color, style and width of the home content.Set up text size to be mobile first. Created group for button for the arrow to point down when hover

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,17 +8,22 @@ const Home = () => {
     <div name="home" className="bg-[#0a192f] w-full h-screen">
       {/* container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <p>Hi! my name is</p>
-        <h1>Michelle von Franzke</h1>
-        <h2>Future Full Stack Developer</h2>
-        <p>
+        <p className='text-yellow-500'>Hi! my name is</p>
+        <h1 className='text-4xl sm:text-5xl font-bold text-[#ccd6f6]'>Michelle von Franzke</h1>
+        <h2 className='text-4xl sm:text-4xl font-bold text-[#8892b0]'>Future Full Stack Developer</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
           My career journey has transitioned from customer service and quality
           assurance to full stack development, driven by a passion for growth,
           commitment to excellence, and a thirst for knowledge.
         </p>
-
         <div>
-          <button>View Work <HiArrowNarrowRight /> </button>
+          <button 
+          className='group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-yellow-600 hover:border-yellow-600'>
+            View Work 
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight />
+            </span>
+            </button>
         </div>
 
       </div>
