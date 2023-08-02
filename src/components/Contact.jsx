0a21a63706605754b5a78daf3fb0babfb467cefb
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 16:02:50 -0700

    set up form, created textbox for email and textarea for body of message

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,6 +22,22 @@ const Contact = () => {
           placeholder="Name"
           name="name"
         />
+
+        <input
+          className="my-4 p-2 bg-[#ccd6f6]"
+          type="email"
+          placeholder="Email"
+          name="email"
+        />
+
+        <textarea
+          className="bg-[#ccd6f6] p-2"
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+
+        
       </form>
     </div>
   );
