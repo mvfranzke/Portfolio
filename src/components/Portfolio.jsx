75849b27b76ccf14b82240a9c0af4f5435253316
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 15:01:16 -0700

    import data from data.js, created portfolio variable that will hold the mapping of all projects from data.js, set classname and style for heading

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { data } from "../data/data.js";
+
 
 const Portfolio = () => {
+  const project = data;
   return (
-    <div>Portfolio</div>
-  )
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-yellow-500">
+            Portfolio
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Portfolio
--- src/data/data.js
+++ src/data/data.js
@@ -9,28 +9,28 @@ export const data = [
     id: 1,
     name: "Recipe Generator",
     image: Recipe,
-    github: "https://github.com/brianlucla/Recipe-Generator-Based-on-Ingredient-Availability"
+    github: "https://github.com/brianlucla/Recipe-Generator-Based-on-Ingredient-Availability",
     live: "https://brianlucla.github.io/Recipe-Generator-Based-on-Ingredient-Availability/"
   },
   {
     id: 2,
     name: "Network Fantasy Trading",
     image: nft,
-    github: "https://github.com/pakuhn93/NFT-marketplace"
+    github: "https://github.com/pakuhn93/NFT-marketplace",
     live: "https://lit-hamlet-47263-654396d0d713.herokuapp.com/marketplace"
   },
   {
     id: 3,
     name: "Project 3",
     image: Recipe,
-    github: ""
+    github: "",
     live: ""
   },
   {
     id: 4,
     name: "Project 4",
     image: nft,
-    github: ""
+    github: "",
     live: ""
   },
 ]
