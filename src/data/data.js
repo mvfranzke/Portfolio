61bd2ea8d7377e705620893963e6cd9c615abe03
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 14:46:31 -0700

    created first item inside data object to hold my first project, holding properties for github repo link and deployed link

--- src/data/data.js
+++ src/data/data.js
@@ -3,4 +3,12 @@
 import Recipe from "../assets/projects/recipe.jpg";
 import nft from "../assets/projects/nft.jpg";
 
-
+export const data = [
+  {
+    id: 1,
+    name: "Recipe Generator",
+    image: Recipe,
+    github: "https://github.com/brianlucla/Recipe-Generator-Based-on-Ingredient-Availability"
+    live: "https://brianlucla.github.io/Recipe-Generator-Based-on-Ingredient-Availability/"
+  },
+]
