cd6590f75d8a09ca7508d78c57deae0b7c886f6a
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 14:14:11 -0700

    imported skills in App.js

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,8 +11,12 @@ import Mongo from "../assets/mongo.png";
 
 const Skills = () => {
   return (
-    <div></div>
-  )
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      
+
+    </div>
+  );
 }
 
 export default Skills
