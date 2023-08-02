e0f2d22c69f18d3265635d987ab91954146c0768
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 15:19:16 -0700

    map data from project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -11,10 +11,18 @@ const Portfolio = () => {
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-yellow-500">
             Portfolio
           </p>
+          <p className="py-6">Below are some of my recent projects</p>
+        </div>
+
+        {/* container box to hold all projects */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* grip display */}
+          {project.map((item, index) => (
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Portfolio
+export default Portfolio;
