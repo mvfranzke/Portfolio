44eb521da30f9b2d79392f16333f94ccc84fa5e8
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 16:01:41 -0700

    set up form, created textbox for name placeholder

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,9 +8,20 @@ const Contact = () => {
     >
       <form method="" action="" className="flex flex-col max-w-[600px] w-full">
         <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-yellow-500 text-gray-300">Contact</p>
-          <p className="text-gray-300 py-4">Please fill out the form below or send me an email at michellevonfranzke@gmail.com</p>
+          <p className="text-4xl font-bold inline border-b-4 border-yellow-500 text-gray-300">
+            Contact
+          </p>
+          <p className="text-gray-300 py-4">
+            Please fill out the form below or send me an email at
+            michellevonfranzke@gmail.com
+          </p>
         </div>
+        <input
+          className="bg-[#ccd6f6] p-2"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
       </form>
     </div>
   );
