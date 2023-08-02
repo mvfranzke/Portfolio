0b5cbbd2f4c2eb2a7cdfcccceed26715ec7aa3bd
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 16:42:32 -0700

    updated href with linkedin account link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,11 +24,31 @@ const Navbar = () => {
 
         {/* navbar menu options */}
         <ul className="hidden md:flex">
-          <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-          <li><Link to="about" smooth={true} duration={500}>About Me</Link></li>
-          <li><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
-          <li><Link to="portfolio" smooth={true} duration={500}>Portfolio</Link></li>
-          <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
+          <li>
+            <Link to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="about" smooth={true} duration={500}>
+              About Me
+            </Link>
+          </li>
+          <li>
+            <Link to="skills" smooth={true} duration={500}>
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link to="portfolio" smooth={true} duration={500}>
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link to="contact" smooth={true} duration={500}>
+              Contact
+            </Link>
+          </li>
         </ul>
 
         {/* additional menu option */}
@@ -46,11 +66,46 @@ const Navbar = () => {
               : "absolute top-0 left-1 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
           }
         >
-          <li className='py-6 text-4xl'><Link to="home" smooth={true} duration={500} onClick={handleClick}>Home</Link></li>
-          <li className='py-6 text-4xl'><Link to="about" smooth={true} duration={500} onClick={handleClick}>About Me</Link></li>
-          <li className='py-6 text-4xl'><Link to="skills" smooth={true} duration={500} onClick={handleClick}>Skills</Link></li>
-          <li className='py-6 text-4xl'><Link to="portfolio" smooth={true} duration={500} onClick={handleClick}>Portfolio</Link></li>
-          <li className='py-6 text-4xl'><Link to="contact" smooth={true} duration={500} onClick={handleClick}>Contact</Link></li>
+          <li className="py-6 text-4xl">
+            <Link to="home" smooth={true} duration={500} onClick={handleClick}>
+              Home
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link to="about" smooth={true} duration={500} onClick={handleClick}>
+              About Me
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link
+              to="skills"
+              smooth={true}
+              duration={500}
+              onClick={handleClick}
+            >
+              Skills
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link
+              to="portfolio"
+              smooth={true}
+              duration={500}
+              onClick={handleClick}
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li className="py-6 text-4xl">
+            <Link
+              to="contact"
+              smooth={true}
+              duration={500}
+              onClick={handleClick}
+            >
+              Contact
+            </Link>
+          </li>
         </ul>
 
         {/* social media icons */}
@@ -59,7 +114,7 @@ const Navbar = () => {
             <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300">
               <a
                 className="flex justify-between items-center w-full text-gray-300"
-                href="/"
+                href="https://www.linkedin.com/in/michelle-entico-006282190/"
               >
                 LinkedIn <FaLinkedin size={30} />
               </a>
