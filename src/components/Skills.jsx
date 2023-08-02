604634df6e5cf3631fce9293583ed2f91b52f914
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 14:32:10 -0700

    added mongo icon image, copied style and hover effects from html

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -58,6 +58,10 @@ const Skills = () => {
             <p className="my-4">NODE JS</p>
           </div>
 
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={Mongo} alt="HTML icon" />
+            <p className="my-4">MONGO DB</p>
+          </div>
           
         </div>
       </div>
