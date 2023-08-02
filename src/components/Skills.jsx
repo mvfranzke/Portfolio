6ebe819045770592b660197b37ef9cf761e055cf
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 14:25:11 -0700

    added html image, set classname to manipulate size, color and on-hover style

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -15,14 +15,26 @@ const Skills = () => {
       {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div>
-          <p className="text-4xl font-bold inline border-b-4 border-yellow-500 ">Skills</p>
-          <p className="py-4">I have experience working with these technologies </p>
+          <p className="text-4xl font-bold inline border-b-4 border-yellow-500 ">
+            Skills
+          </p>
+          <p className="py-4">
+            I have experience working with these technologies{" "}
+          </p>
         </div>
 
-        
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+            <p className="my-4">HTML</p>
+          </div>
 
 
 
+
+
+        </div>
       </div>
     </div>
   );
