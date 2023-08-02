4042dede43c8bea850b4aba7ff3066370c23fb78
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 12:02:27 -0700

    adjusted classname to center and make font bigger for the about me content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,19 +12,23 @@ const About = () => {
           </div>
           <div></div>
         </div>
-        <p>
-          Hi. I'm Michelle, thank you for visiting my page. Please take a look
-          around.
-        </p>
-        <div className="sm:text-right text-4xl font-bold">
-          <p>
-            My evolution from customer service and quality assurance to full
-            stack development has been driven by a passion for growth, a
-            commitment to excellence, and a thirst for knowledge. I am excited
-            to bring my diverse background and technical proficiency to the
-            forefront of software development, ready to create exceptional
-            solutions and make a meaningful impact in this dynamic field.
-          </p>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-3xl font-bold">
+            <p>
+              Hi. I'm Michelle, thank you for visiting my page. Please take a
+              look around.
+            </p>
+          </div>
+          <div>
+            <p>
+              My evolution from customer service and quality assurance to full
+              stack development has been driven by a passion for growth, a
+              commitment to excellence, and a thirst for knowledge. I am excited
+              to bring my diverse background and technical proficiency to the
+              forefront of software development, ready to create exceptional
+              solutions and make a meaningful impact in this dynamic field.
+            </p>
+          </div>
         </div>
       </div>
     </div>
