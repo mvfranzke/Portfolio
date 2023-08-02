98c6d9379ac22f80daff147b3f6f739984bdc68f
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 15:21:02 -0700

    render display for project image

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -18,6 +18,14 @@ const Portfolio = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* grip display */}
           {project.map((item, index) => (
+            <div
+              key={index}
+              style={{ backgroundImage: `url(${item.image})` }}
+              className="shadow-lg shadow-[#040c16] group container rounded-md 
+              flex justify-center text-center items-center mx-auto content-div ">
+              {/* on hover style*/}
+              
+            </div>
           ))}
         </div>
       </div>
