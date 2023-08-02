b08a5246bb4fe3dd015241363114ca1bd201c51c
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 14:22:38 -0700

    set classname for heading title on skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import HTML from "../assets/html.png";
 import CSS from "../assets/css.png";
 import JavaScript from "../assets/javascript.png";
-import ReactImg from "../assets/react.png";
+import ReactImg from "../assets/reactImg.png";
 import Node from "../assets/node.png";
 import GitHub from "../assets/github.png";
 import Tailwind from "../assets/tailwind.png";
@@ -11,10 +11,19 @@ import Mongo from "../assets/mongo.png";
 
 const Skills = () => {
   return (
-    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
       {/* Container */}
-      
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-yellow-500 ">Skills</p>
+          <p className="py-4">I have experience working with these technologies </p>
+        </div>
 
+        
+
+
+
+      </div>
     </div>
   );
 }
