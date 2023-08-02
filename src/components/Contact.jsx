58e3590b6075ac485cf492fc0dc93baa3b474efb
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 16:14:12 -0700

    added method post and action link from my getform.io account to receive future message and for user to get an automated response

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,11 @@ const Contact = () => {
       name="contact"
       className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
     >
-      <form method="" action="" className="flex flex-col max-w-[600px] w-full">
+      <form
+        method="POST"
+        action="https://getform.io/f/9a4e704b-05ec-4b09-82ac-8456271ce5b9"
+        className="flex flex-col max-w-[600px] w-full"
+      >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-yellow-500 text-gray-300">
             Contact
@@ -40,7 +44,6 @@ const Contact = () => {
         <button className="text-white border-2 hover:bg-yellow-500 hover:border-yellow-500 px-4 py-3 my-8 mx-auto flex items-center">
           Let's work on a project!
         </button>
-        
       </form>
     </div>
   );
