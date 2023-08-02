2ec41e1b5a389112163316dd458fea20680def86
mvfranzke <michellevonfranzke@gmail.com>
2023-08-02 15:59:40 -0700

    created heading for contact, set classname

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
+    <div
+      name="contact"
+      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+    >
+      <form method="" action="" className="flex flex-col max-w-[600px] w-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-yellow-500 text-gray-300">Contact</p>
+          <p className="text-gray-300 py-4">Please fill out the form below or send me an email at michellevonfranzke@gmail.com</p>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Contact
--- src/data/data.js
+++ src/data/data.js
@@ -9,28 +9,43 @@ export const data = [
     id: 1,
     name: "Recipe Generator",
     image: Recipe,
-    github: "https://github.com/brianlucla/Recipe-Generator-Based-on-Ingredient-Availability",
-    live: "https://brianlucla.github.io/Recipe-Generator-Based-on-Ingredient-Availability/"
+    github:
+      "https://github.com/brianlucla/Recipe-Generator-Based-on-Ingredient-Availability",
+    live: "https://brianlucla.github.io/Recipe-Generator-Based-on-Ingredient-Availability/",
   },
   {
     id: 2,
     name: "Network Fantasy Trading",
     image: nft,
     github: "https://github.com/pakuhn93/NFT-marketplace",
-    live: "https://lit-hamlet-47263-654396d0d713.herokuapp.com/marketplace"
+    live: "https://lit-hamlet-47263-654396d0d713.herokuapp.com/marketplace",
   },
   {
     id: 3,
     name: "Project 3",
     image: Recipe,
     github: "",
-    live: ""
+    live: "",
   },
   {
     id: 4,
     name: "Project 4",
     image: nft,
     github: "",
-    live: ""
+    live: "",
   },
-]
+  {
+    id: 5,
+    name: "Project 5",
+    image: Recipe,
+    github: "",
+    live: "",
+  },
+  {
+    id: 6,
+    name: "Project 6",
+    image: nft,
+    github: "",
+    live: "",
+  }
+];
